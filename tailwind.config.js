6904e0f83e999f5d9963a9840ec98ff67e68ca8b
Steph Dmts <stephane.diamantopoulos@gmail.com>
2023-05-18 10:05:25 +0200

    extend tailwind with custom fonts shadows and brightness options

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{js,jsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'kanit': ['Kanit']
+      },
+      boxShadow: {
+        'thumbnail': '0 7px 20px 0 rgba(202,203,207,0.5)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      },
+      brightness: {
+        25: '.25',
+        75: '.75'
+      }
+    },
   },
   plugins: [],
 }
