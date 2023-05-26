724b280cb7a825ad4fa4f5a63e23ae7d181a54e4
Stephane Diamantopoulos <stephane.diamantopoulos@gmail.com>
2023-05-26 12:32:17 +0300

    add google books embedded viewer api cdn and init reader

--- src/Ereader.jsx
+++ src/Ereader.jsx
@@ -1,19 +1,30 @@
 import { useRef } from "react";
-
+import { useParams } from "react-router-dom";
 
 const Ereader = () => {
+  const { id } = useParams();
+  
   const viewerCanvas = useRef(null);
+  // eslint-disable-next-line no-undef
   google.books.load();
 
   function initialize() {
+    // eslint-disable-next-line no-undef
     var viewer = new google.books.DefaultViewer(
       document.getElementById("viewerCanvas")
     );
-    viewer.load("ISBN:0738531367");
+    viewer.load(id);
   }
 
+  // eslint-disable-next-line no-undef
   google.books.setOnLoadCallback(initialize);
-  return <div ref={viewerCanvas} id="viewerCanvas" style={{width: "1000px", height: "700px"}}></div>;
+  return (
+    <div
+      ref={viewerCanvas}
+      id="viewerCanvas"
+      style={{ width: "1000px", height: "700px" }}
+    ></div>
+  );
 };
 
 export default Ereader;
