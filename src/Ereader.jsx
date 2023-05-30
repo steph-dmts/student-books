fb87138502755d94d3a059e2aae2cc812b74379b
Steph Dmts <stephane.diamantopoulos@gmail.com>
2023-05-30 12:31:57 +0200

    add useeffect hook to make sure the google api is laoded before rendering the viewer

--- src/Ereader.jsx
+++ src/Ereader.jsx
@@ -1,29 +1,53 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Ereader = () => {
   const { id } = useParams();
-  
-  const viewerCanvas = useRef(null);
-  // eslint-disable-next-line no-undef
-  google.books.load();
+  const [loaded, setLoaded] = useState(false);
+  const viewerCanvas = useRef();
 
-  function initialize() {
-    // eslint-disable-next-line no-undef
-    var viewer = new google.books.DefaultViewer(
-      document.getElementById("viewerCanvas")
-    );
-    viewer.load(id);
-  }
+  useEffect(() => {
+    const scriptTag = document.createElement("script");
+    scriptTag.src = "https://www.google.com/books/jsapi.js";
+    scriptTag.addEventListener("load", () => setLoaded(true));
+    scriptTag.id = "google-script";
+    document.body.appendChild(scriptTag);
+  }, []);
 
-  // eslint-disable-next-line no-undef
-  google.books.setOnLoadCallback(initialize);
+  useEffect(() => {
+    if (!loaded) return;
+    else {
+      if (window.viewer) {
+        let viewer = new window.google.books.DefaultViewer(
+          viewerCanvas.current
+        );
+        viewer.load(id);
+      } else {
+        window.google.books.load();
+        window.google.books.setOnLoadCallback(() => {
+          let viewer = new window.google.books.DefaultViewer(
+            viewerCanvas.current
+          );
+          window.viewer = viewer;
+          viewer.load(id);
+        });
+      }
+    }
+  }, [loaded]);
   return (
-    <div
-      ref={viewerCanvas}
-      id="viewerCanvas"
-      style={{ width: "1000px", height: "700px" }}
-    ></div>
+    <div>
+      {loaded ? (
+        <div>
+          <div
+            ref={viewerCanvas}
+            id="viewerCanvas"
+            style={{ width: "1000px", height: "700px" }}
+          ></div>
+        </div>
+      ) : (
+        "Script not loaded"
+      )}
+    </div>
   );
 };
 
