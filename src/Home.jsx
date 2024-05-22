adb7b85e59c7fb1f5123928ff90634a46b3cc899
Steph <157031062+steph-m-work@users.noreply.github.com>
2024-05-22 09:42:02 +0200

    [update] change bg color

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="grid grid-cols-[240px_1fr] grid-rows-[1fr] h-screen ">
         <Sidebar />
-      <div className="">
+      <div className="bg-slate-200">
         <SearchResults />
       </div>
     </div>
